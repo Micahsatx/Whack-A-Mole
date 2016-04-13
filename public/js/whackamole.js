b088821544028b3399b1d7a0c02a44147a35dc5b
micahbohemian <Micahjohnson1983@gmail.com>
2016-04-13 09:32:59 -0500

    second draft.  fully functional, style added, and features included.  still needs final touches.  code needs cleaned up, notes need updated

--- public/js/whackamole.js
+++ public/js/whackamole.js
@@ -1,35 +1,42 @@
 // (function(){
 "use strict"
-
-var index = 0;
+// global value that is set equal to random button in the newRandomButton
+// newRandomButton returns a value for buttonChosen
 var buttonChosen;
+// setting initial value's for values used.
 var score = 0;
+var highScore = 0;
 var timer = 30;
 // this variable is tied to the click listener that checks if it is correct 
-var box = $(".individualBox");
-
+// can this variable box be cut out in someway to shorten/clean up code
+var box = $(".flower");
+var moleInt;
+var intervalTime = 1500;
 
 
 // function that chooses a random button and assigns it to buttonChosen
     function newRandomButton(){
         // find a random button
-        var $keys = $('.individualBox');
+        var $keys = $('.flower');
         var buttonChosen = $keys[Math.floor(Math.random()*$keys.length)];
-        moleInSingleButton();
-        // console.log(buttonChosen);
-
+        // set the value for button chosen so it can be used if start button is used.
         return buttonChosen;
     };
-
+    // one of these random box variable usages that will likely be changed.
     box.click(function(event){
             console.log(buttonChosen);
             if ( this == buttonChosen ){
                 console.log("CORRECT");
+                // when clicked correctly add 1 to the current score.  starts at 0 because
+                // of global variable value set at the start
                 score++;
                 $("#score").html(score);
-               
-
-            // this is what runs when the sequence fails 
+                // if the score is greater than the highscore then replace the previous highscore
+                if(score > highScore){
+                    highScore = score;
+                    // what actually appends the html of the highscore.
+                    $("#highScore").html(highScore);
+                }
             } else {
                 console.log("WRONG")
             }
@@ -37,32 +44,35 @@ var box = $(".individualBox");
 
 
     function moleInSingleButton(button) {
-        $(button).fadeTo("fast", 0.1);
-        $(button).fadeTo("fast", 1.0);
-        // unsure but i think animating goes here...
-        // $(selector).animate({params},speed,callback);
-        // $("button").click(function(){
-        //     $("div").animate({left: '250px'});
-        // }); 
+            $(button).animate({
+                'margin-top': '-=50px'
+            }, (intervalTime / 2)).delay(1000).animate({
+                'margin-top': '+=50px'
+            });
+            console.log($(button));
     }
 
-
+    // this likely can be chopped into 2 function so that it is shorter.  start button
+    // runs countDown function
+    // then the interval uses buttonChosen(the random button/hole)
+    // puts the random button/hole into moleInSingleButton that animates a specific random button
     $("#idOfStart").click(function(){
             countDown();
-        setInterval(function() {
+            moleInt = setInterval(function() {
             buttonChosen = newRandomButton();
             moleInSingleButton(buttonChosen);
-        }, 1500);
+        }, intervalTime);
     });
 
 
-
+    // the countDown function.  starts at 30seconds then lowers the count by 1
+    // at an interval or aprox. 1 second.
     function countDown(){
         var timeInt = setInterval(function(){
             timer--;
             $("#timer").html(timer);
-            // onRound();
             if(timer == 0){
+                clearInterval(moleInt);
                 clearInterval(timeInt);
                 alert("Game Over!")
             }
